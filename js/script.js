41f8deea0dd4571453a22e1038b2ffd87dbd59ea
Amjed Saleel K <amjedsaleel@gmail.com>
2020-06-21 16:19:38 +0530

    form submission

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ $(function () {
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
   });
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbw_p6XKgnCtAyW0FC_bdC_oFy4iKiNxQ2QX_SltpDcPC1HGtJI/exec'
+    const form = document.forms['submit-to-google-sheet']
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message))
+        alert('Thanks...')
+    })
